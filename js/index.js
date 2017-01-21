c5250ee4d3d7c5ec9f7cd456363fb2d995c90bd2
zhaojunming <zhao>
2017-01-21 15:00:54 +0800

    init

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
 /**
  * Created by zhaojunming on 16/12/31.
  */
+
+function playAudioCanClick() {
+    document.getElementById("AUDIO_CAN_CLICK").play();
+}
+
+function playAudioCanNotClick() {
+    document.getElementById("AUDIO_CAN_NOT_CLICK").play();
+}
+
+$(function () {
+
+    $("body").click(function (event) {
+        if (event.target.dataset.canClick) {
+            playAudioCanClick();
+        } else {
+            playAudioCanNotClick();
+        }
+    });
+
+
+});
